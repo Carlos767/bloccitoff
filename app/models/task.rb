fa9e7942cf08ff2019a76a813665041cdc5bf871
Carlos767 <cjparrillasf@gmail.com>
2015-08-27 07:31:53 -0700

    Added length and presence validation to task list title

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,25 @@
 class Task < ActiveRecord::Base
+	validate :title, presence: true
+	validates :title, length: { minimum: 3 }
 end
+
+# view
+# <%= link_to "Complete", user_task_complete_path(current_user, task), method: :put %>
+# <%= task.completed ? "Completed" : "Pending" %>
+# Expires in: <%= distance_of_time_in_words_to_now(task.expires_at) %>
+
+# routes
+# resources :tasks do
+#	put :complete
+# end
+
+# tasks controller
+# def complete
+#    @task = Task.find(params[:task_id])
+	#if task is complete, then make complete false, else make it true. then, redirect_to somewhere
+# end
+
+# task
+# expires_at:datetime
+# completed:boolean
+# git rm --cached config/application.yml
