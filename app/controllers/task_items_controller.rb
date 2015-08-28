8f30eba892ab698b6a18d1f11541cc6b5ca9b6ee
Carlos767 <cjparrillasf@gmail.com>
2015-08-28 10:08:02 -0700

    Witing tests for todo items

--- app/controllers/task_items_controller.rb
+++ app/controllers/task_items_controller.rb
@@ -1,4 +1,5 @@
 class TaskItemsController < ApplicationController
   def index
+  	@task = TodoList.find(params[:task_id])
   end
 end
--- spec/features/task_items/index_spec.rb
+++ spec/features/task_items/index_spec.rb
@@ -2,12 +2,39 @@ require 'spec_helper'
 
 describe "Viewing task items" do
 	let!(:task) { Task.create(title: "Grocery list", description "Groceries") }
-	it "displays no items when a todo list s empty" do
+
+	def visit_task(list)
 		visit "/tasks"
-		within "#task_#{task.id}" do
+		within "#task_#{list.id}" do
 			click_link "List Items"
 		end
-		expect(page).to have_content("TaskItems#index")
+	end
+
+
+	it "displays the title of the todo list" do
+		visit_task(task)
+		within("h1") do
+			expect(page).to have_content(task.title)
+		end
+	end
+
+	it "displays no items when a todo list s empty" do
+		visit_task(task)
+		expect(page.all("ul.task_items li").size).to eq(0)
+	end
+
+	it "displays item content when a todo list has items" do
+		task.tasks.create(content: "Butter")
+		task.tasks.create(content: "Bacon")
+		
+		visit_task(task)
+		
+		expect(page.all("ul.task_items li").size).to eq(2)
+
+		within "ul.task_items" do
+			expect(page).to have_content("Butter")
+			expect(page).to have_content("Bacon")
+		end
 	end
 	
 end
